82b5b859cd3c3644999624365520fdf22549e3c1
codeprismtechnologies <chandan.rai@codeprism.in>
2023-03-02 15:54:20 +0530

    Initial commit

---
+++ src/main/java/com/demo/app/demowebsocketapp/DTO/TextMessageDTO.java
@@ -0,0 +1,15 @@
+package com.demo.app.demowebsocketapp.DTO;
+
+public class TextMessageDTO {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
---
+++ src/main/java/com/demo/app/demowebsocketapp/DemoWebsocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.app.demowebsocketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebsocketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebsocketAppApplication.class, args);
+    }
+
+}
